87b0870e1a98c63c7c31d8c8feb255a85f490a5c
Dennelle <dennelle.walton@gmail.com>
2023-11-02 10:00:45 -0400

    add logo

--- routes/travels.js
+++ routes/travels.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const travelsCtrl = require('../controllers/travels')
+const isLoggedIn = require('../config/auth')
 
-router.get('/travels', travelsCtrl.index);
-router.post('/travels', travelsCtrl.create);
-router.delete('/travels/:id', travelsCtrl.delete);
-router.get('/travels/new', travelsCtrl.new);
-router.get('/travels/:id', travelsCtrl.show);
-router.get('/travels/:id/edit', travelsCtrl.edit);
-router.put('/travels/:id', travelsCtrl.update);
+router.get('/travels', isLoggedIn, travelsCtrl.index);
+router.post('/travels', isLoggedIn, travelsCtrl.create);
+router.delete('/travels/:id', isLoggedIn, travelsCtrl.delete);
+router.get('/travels/new', isLoggedIn, travelsCtrl.new);
+router.get('/travels/:id', isLoggedIn, travelsCtrl.show);
+router.get('/travels/:id/edit', isLoggedIn, travelsCtrl.edit);
+router.put('/travels/:id', isLoggedIn, travelsCtrl.update);
 
 module.exports = router;
